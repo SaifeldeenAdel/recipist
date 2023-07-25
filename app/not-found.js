1e0072e59f3e05f43475a35695800be17bfb303f
Saif eldeen Adel <saifadel77@hotmail.com>
2023-07-25 14:34:58 +0300

    :bug: Fix deployment bug

--- app/not-found.js
+++ app/not-found.js
@@ -6,7 +6,7 @@ export default function NotFound() {
 		<div className="flex flex-col gap-6 justify-center items-center h-[90vh]">
 			<Image alt="404 Icon" src={NotFoundSVG} className="w-[12rem] sm:w-[15rem]" />
 			<p className="font-sans text-[1.2rem] sm:text-[1.5rem] font-medium">
-				What're you doing here?
+				What&apos;re you doing here?
 			</p>
 		</div>
 	);
