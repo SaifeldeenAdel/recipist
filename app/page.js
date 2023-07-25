fded831583e432fc20d85fc73aed0495e07f2d5e
Saif eldeen Adel <saifadel77@hotmail.com>
2023-07-25 14:41:08 +0300

    :bug: Fix dynamic deployment bug

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Recipes from "@/components/recipes/Recipes";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
+export const dynamic = 'force-dynamic';
 export default async function Home() {
 	const supabase = createServerComponentClient({ cookies });
 	const {
