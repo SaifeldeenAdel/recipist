4d32f74cafbdb0406693f11aa298f522866809fc
Saif eldeen Adel <saifadel77@hotmail.com>
2023-08-09 13:53:55 +0300

    Fixed nav z-index, edited scrollbar design

--- components/nav/Nav.js
+++ components/nav/Nav.js
@@ -1,6 +1,6 @@
 export default function Nav() {
 	return (
-		<nav className="fixed top-0 w-full flex items-center justify-center h-[4rem] bg-white shadow-md">
+		<nav className="fixed top-0 w-full flex items-center justify-center h-[4rem] bg-white shadow-md z-[1000]">
 			<div className="flex flex-row justify-between items-center w-[65rem] mx-4">
 				<h1 className="font-logo text-primary text-[2rem] font-black">
 					Recipist
